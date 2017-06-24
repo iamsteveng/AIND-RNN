2795c692a98a7227df9fe1072aff0efad8f5e7e9
Steve Ng <iamsteveng@gmail.com>
2017-06-24 09:31:52 +0000

    Finished TODO#1

--- my_answers.py
+++ my_answers.py
@@ -12,13 +12,27 @@ def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    
+    # get the length of the series
+    series_len = len(series)
+    
+    # iterate from index 0
+    for idx, val in enumerate(series):
+        # check whether the end is reached or not, if yes, then leave the loop
+        if ((idx+window_size) >= series_len):
+            break
+        # get the series values of window size
+        X.append(series[idx:idx+window_size])
+        # get the series value after the window size
+        y.append(series[idx+window_size])
+    
+        
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-    
+     
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
